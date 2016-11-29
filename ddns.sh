3d53893fb62a2b051e69861ab92bd369d967ce57
Kieran Colford <kieran@kcolford.com>
2016-11-29 18:45:00 -0500

    added support for SSHFP records

--- ddns.sh
+++ ddns.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
-ip="$(curl -6 icanhazip.com)"
-echo "Your IPv6 address is $ip"
+ipurl="icanhazip.com"
 
 for key in /etc/ddns/*
 do
     host="$(sed 's/key "\([a-z.]*\)".*/\1/' "$key" | head -1)"
     echo "Updating host $host"
-    nsupdate -k "$key" <<EOF
-update delete $host AAAA
-update add $host 300 AAAA $ip
-send
+    {
+	echo "ttl 300"
+	if curl -6 "$ipurl" > /dev/null
+	then
+	    echo "del $host AAAA"
+	    echo "add $host AAAA $(curl -6 "$ipurl")"
+	fi
 
-EOF
+	if ssh-keygen -r "$host" > /dev/null
+	then
+	    echo "del $host SSHFP"
+	    ssh-keygen -r "$host" | sed 's/.*/add &/'
+	fi
+
+	echo "show"
+	echo "send"
+    } | nsupdate -k "$key"
 done
-
