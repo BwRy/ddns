e577415d7bdba1321361d55d49f3742dc36f00a4
Kieran Colford <kieran@kcolford.com>
2016-11-30 12:08:25 -0500

    fixed up SSHFP updates

--- ddns.sh
+++ ddns.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 ipurl="icanhazip.com"
+ttl=300
 
 for key in /etc/ddns/*
 do
     host="$(sed 's/key "\([a-z.]*\)".*/\1/' "$key" | head -1)"
     echo "Updating host $host"
     {
-	echo "ttl 300"
+	echo "ttl $ttl"
 	if curl -6 "$ipurl" > /dev/null
 	then
 	    echo "del $host AAAA"
-	    echo "add $host AAAA $(curl -6 "$ipurl")"
+	    echo "add $host $ttl AAAA $(curl -6 "$ipurl")"
 	fi
 
 	if ssh-keygen -r "$host" > /dev/null
 	then
 	    echo "del $host SSHFP"
-	    ssh-keygen -r "$host" | sed 's/.*/add &/'
+	    ssh-keygen -r "" | sed "s/.*/add $host $ttl &/"
 	fi
 
 	echo "show"
