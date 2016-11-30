f84986173e58aaf4984396c01a26bf0ba65a98e7
Kieran Colford <kieran@kcolford.com>
2016-11-30 12:51:53 -0500

    updated remote

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ updpkgsums
 makepkg --printsrcinfo > .SRCINFO
 git commit -m 'update PKGBUILD' PKGBUILD .SRCINFO
 cd ..
-url=aur
+url=ssh://aur@aur.archlinux.org/ddns.git
 git fetch $url
 git subtree push -P aur $url master
